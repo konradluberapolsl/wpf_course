821b0c38f80c58d119a967508e87b6048b0a8a3b
Konrad <konrlub566@polsl.pl>
2020-03-14 00:11:14 +0100

    First app added

---
+++ Lab1/Lab1/texBoxWithErrorProvider.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Lab1
+{
+    /// <summary>
+    /// Logika interakcji dla klasy texBoxWithErrorProvider.xaml
+    /// </summary>
+
+
+    public partial class texBoxWithErrorProvider : UserControl
+    {
+        #region wlasciwosci
+        public Brush TextBoxBorderBrush { get { return border.BorderBrush; }  set { border.BorderBrush = value; }  }
+        public string Text { get { return textBox.Text; } set { textBox.Text = value; } }
+        #endregion
+
+        public texBoxWithErrorProvider()
+        {
+            InitializeComponent();
+        }
+
+        public void SetError(string errorText)
+        {
+            if (errorText == "")
+            {
+                border.BorderThickness = new Thickness(1);
+                textBoxtToolTip.Visibility = Visibility.Visible;
+                border.BorderBrush = Brushes.Red;
+            }
+               
+            else
+                border.BorderThickness = new Thickness(0);
+        }
+    }
+}
